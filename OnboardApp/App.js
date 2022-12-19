b2fe62252cbc88795e6b4cf7a0f012e726e89a77
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-19 19:16:11 +0530

    Setting the basic UI design for the first page of my app

--- OnboardApp/App.js
+++ OnboardApp/App.js
@@ -1,23 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import colors from './assets/colors/colors';
-import fonts from './assets/fonts';
+import React from 'react';
+
+import {View, Text, Image, StyleSheet, StatusBar} from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+const data = [
+  {
+    title: 'Save time by tracking your studies ',
+    text: 'Schedule your classes, assignments, quizzes and more',
+    image: require('./assets/images/Onboard_1.png'),
+    
+  },
+  {
+    title: 'Stay on top of your education',
+    text: 'Reduce your stress, increase your productivity',
+    image: require('./assets/images/Onboard_2.png'),
+    
+  },
+  {
+    title: 'Spend more time doing the things you love',
+    text: "Get started within five minutes",
+    image: require('./assets/images/Onboard_3.png'),
+    
+  },
+];
+
 
 
 export default function App() {
+
+  const renderItem=({item})=>{
+    return(
+      <Text>Onboard Screen</Text>
+    )
+  }
+
+  const keyExtractor=(item)=>item.title;
   return (
-    <View style={styles.container}>
-      <Text>i love you mom!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{flex: 1}}>
+    <StatusBar translucent backgroundColor="transparent" />
+    <AppIntroSlider
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+      data={data}
+    />
+  </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ OnboardApp/components/Onboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Onboard = () =>{
+    return (
+        <View>
+            <Text>Onboard !!</Text>
+        </View>
+    )
+}
+export default Onboard;
