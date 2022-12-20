f004fdbd5893c768598f3b064c89ce74f3250d37
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-20 18:38:58 +0530

    Completed designing the UI of the app

--- OnboardApp/assets/colors.js
+++ OnboardApp/assets/colors.js
@@ -2,7 +2,8 @@ const colors={
     black:'#000000',
     gray:'#666666',
     blue:'#1C215D',
-    white:'#ffffff'
+    bluefaded:'#8D8FAD',
+    white:'#ffffff',        
 
 }
 export default colors;
--- OnboardApp/components/Onboard.js
+++ OnboardApp/components/Onboard.js
@@ -1,11 +1,147 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import colors from './assets/colors';
 
-const Onboard = () =>{
-    return (
+
+import {View, Text, Image, StyleSheet, StatusBar} from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+
+const data = [
+  {
+    title: 'Save time by tracking your studies ',
+    text: 'Schedule your classes, assignments, quizzes and more',
+    image: require('./assets/images/Onboard_1.png'),
+    
+  },
+  {
+    title: 'Stay on top of your education',
+    text: 'Reduce your stress, increase your productivity',
+    image: require('./assets/images/Onboard_2.png'),
+    
+  },
+  {
+    title: 'Spend more time doing the things you love',
+    text: "Get started within five minutes",
+    image: require('./assets/images/Onboard_3.png'),
+    
+  },
+];
+
+
+
+export default function OnboardApp() {
+
+  const renderItem=({item})=>{
+    return(
+      
+       
+      
+      <View style={styles.slide}>
+      
+       <Image source={item.image} style={styles.image} />
         <View>
-            <Text>Onboard !!</Text>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.text}>{item.text}</Text>
         </View>
+        
+      </View>
     )
+  }
+
+  const keyExtractor=(item)=>item.title;
+  const renderDoneButton=()=>{
+    return(
+      <View style={styles.rightTextWrapper}>
+        <Text style={styles.rightText}>Done</Text>
+      </View>
+      );
+  };
+  const renderNextButton=()=>{
+    return(
+      <View style={styles.rightTextWrapper}>
+        <Text style={styles.rightText}>Next</Text>
+      </View>
+      );
+  };
+  const renderPrevButton=()=>{
+    return(
+      <View style={styles.leftTextWrapper}>
+        <Text style={styles.leftText}>Prev</Text>
+      </View>
+      );
+  };
+  return (
+    <View style={{flex: 1}}>
+    <StatusBar translucent backgroundColor="transparent" />
+    <AppIntroSlider
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+      renderDoneButton={renderDoneButton}
+      renderNextButton={renderNextButton}
+      renderPrevButton={renderPrevButton}
+      showPrevButton
+      dotStyle={styles.dotStyle}
+      activeDotStyle={styles.activeDotStyle}
+      data={data}
+    />
+  </View>
+  );
 }
-export default Onboard;
+
+const styles=StyleSheet.create({
+
+  slide:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:colors.white,
+
+
+  },
+  image:{
+    marginVertical:60,
+  },
+  title:{
+    fontSize:24,
+    color:colors.black,
+    textAlign:'center',
+    //fontFamily:'OpenSans-Bold',
+    marginHorizontal:60,
+
+  },
+  text:{
+    fontSize:14,
+    color:colors.gray,
+    textAlign:'center',
+    //fontFamily:'OpenSans-SemiBold',
+    marginHorizontal:60,
+  },
+  rightTextWrapper:{
+    width:40,
+    height:40,
+    marginRight:20,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  leftTextWrapper:{
+    width:40,
+    height:40,
+    marginLeft:20,
+    justifyContent:'center',
+    alignItems:'center',},
+  rightText:{
+    colors:colors.gray,
+    fontSize:14,
+  },
+  leftText:{
+    colors:colors.gray,
+    fontSize:14,
+  },
+  dotStyle:{
+    backgroundColor:colors.bluefaded,
+  },
+  activeDotStyle:{
+    backgroundColor:colors.blue,
+  },
+});
+
