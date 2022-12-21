789a5e72b93861c600693be44d29f7e9cc11e174
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-21 20:01:05 +0530

    Setting fonts and colors folder

--- Foodappui/App.js
+++ Foodappui/App.js
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import colors from './assets/colors';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-SemiBold': require('./assets/fonts/Montserrat-SemiBold.ttf')
+  });
+
+  // ...
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -16,5 +28,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily:'Montserrat-SemiBold',
+    color:'#F26C68',
   },
 });
