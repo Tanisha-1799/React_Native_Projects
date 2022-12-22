6f3024d8cf1bd66f1373ea4168426605a7c62902
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-22 20:37:34 +0530

    Adding data folder to the assets folder

---
+++ Foodappui/assets/data/popularData.js
@@ -0,0 +1,23 @@
+const popularData=[
+    {
+        id:1,
+        image:require('../images/pizza1.png'),
+        title:'Primavera Pizza',
+        weight:'540 gm',
+        rating:'5.0',
+    },
+    {
+        id:1,
+        image:require('../images/pizza2.png'),
+        title:'Vegetarian Pizza',
+        weight:'540 gm',
+        rating:'5.0',
+    },
+    {
+        id:1,
+        image:require('../images/pizza3.png'),
+        title:'Pepperoni Pizza',
+        weight:'540 gm',
+        rating:'5.0',
+    }
+];
