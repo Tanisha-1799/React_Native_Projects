d92244fd14d8e3bace7f130771ab38e52170626b
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-22 20:33:49 +0530

    Adding data folder to the assets folder

---
+++ Foodappui/assets/data/categoriesData.js
@@ -0,0 +1,22 @@
+const categoriesData=[
+{
+    id:1,
+    image: require('../images/pizza-icon.png'),
+    title:'Pizza',
+    selected:true,
+},
+{
+    id:1,
+    image: require('../images/shrimp-icon.png'),
+    title:'Sea Food',
+    selected:true,
+},
+{
+    id:1,
+    image: require('../images/soda-icon.png'),
+    title:'Soft Drinks',
+    selected:true,
+},
+
+];
+export default categoriesData;
