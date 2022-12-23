b26bf77d00d57cf83192aeb94b12ac1d00968a75
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-23 20:05:45 +0530

    designing the first page of the app

--- Foodappui/components/Home.js
+++ Foodappui/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import categoriesData from '../assets/data/categoriesData';
@@ -18,6 +18,25 @@ const Home =()=>{
                     
                 </View>
             </SafeAreaView>
+            <View style={styles.titleWrapper}>
+                <Text style={styles.titleSubtitle}>Food</Text>
+                <Text style={styles.titleTitle}>Delivery</Text>
+            </View>
+            {/* Search */}
+            <View style={styles.searchWrapper}>
+                <Feather name="search" size={16} color={colors.textDark} />
+                <View style={styles.search}>
+                    <Text style={styles.searchText}>Search</Text>
+                </View>
+            </View>
+
+            {/* Categories */}
+            <View style={styles.categoriesWrapper}>
+                <Text style={styles.categoriesTitle}>Categories</Text>
+                <View style={styles.categoriesListWrapper}>
+                    <FlatList />
+                </View>
+            </View>
             
         </View>
     );
@@ -25,16 +44,63 @@ const Home =()=>{
 export default Home;
 const styles=StyleSheet.create({
     container:{
-        paddingTop:40,
+       
         flex:1,
     },
     headerWrapper:{
         flexDirection:'row',
         justifyContent:'space-between',
         paddingHorizontal:20,
+        paddingTop:40,
+        alignItems:'center',
     },
     profileImage:{
-        width:50,
-        height:50,
+        width:40,
+        height:40,
+        borderRadius:40,
     },
+    titleWrapper:{
+        marginTop:20,
+        paddingHorizontal:20,
+    },
+    titleSubtitle:{
+        fontFamily:'MontserratRegular',
+        fontSize:16,
+        color:colors.textDark,
+    },
+    titleTitle:{
+        fontFamily:'MontserratBold',
+        fontSize:32,
+        color:colors.textDark,
+        
+    },
+    searchWrapper:{
+        flexDirection:'row',
+        alignItems:'center',
+        paddingHorizontal:20,
+        marginTop:20,
+    },
+    search:{
+        flex:1,
+        marginLeft:10,
+        borderBottomColor:colors.textLight,
+        borderBottomWidth:2,
+    },
+    searchText:{
+        fontFamily:'MontserratSemiBold',
+        fontSize:14,
+        color:colors.textLight,
+        marginBottom:5,
+    },
+    categoriesWrapper:{
+        marginTop:30,
+        marginHorizontal:20,
+    },
+    categoriesTitle:{
+        fontFamily:'MontserratBold',
+        fontSize:16,
+        color:colors.textDark,
+        
+    },
+    categoriesListWrapper:{},
 });
