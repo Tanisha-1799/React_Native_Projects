47f8b8d3db0b7d5c034b587a5d3ce007b7c00b72
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-23 19:15:55 +0530

    designing the first page of the app

--- Foodappui/components/Home.js
+++ Foodappui/components/Home.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import categoriesData from '../assets/data/categoriesData';
+import popularData from '../assets/data/popularData';
+import colors from '../assets/colors';
 
+Feather.loadFont
 const Home =()=>{
     return (
-        <View style={styles.title}>
-            <Text style={styles.text}>Hello to home</Text>
+        <View style={styles.container}>
+            <SafeAreaView>
+                <View style={styles.headerWrapper}>
+                    <Image source={require('../assets/images/prr2.jpg')} 
+                    style={styles.profileImage}/>
+                    <Feather name='menu' size={24} color={colors.textDark} />
+                    
+                </View>
+            </SafeAreaView>
+            
         </View>
     );
 }
 export default Home;
 const styles=StyleSheet.create({
-    text:{
-        fontFamily:"MontserratBold",
-        color:'000000',    
+    container:{
+        paddingTop:40,
+        flex:1,
+    },
+    headerWrapper:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingHorizontal:20,
+    },
+    profileImage:{
+        width:50,
+        height:50,
     },
 });
