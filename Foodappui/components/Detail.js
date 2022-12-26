142cba42cea49c9aef939fde9782dff5df71bf73
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-26 18:25:51 +0530

    designing the detais page

--- Foodappui/components/Detail.js
+++ Foodappui/components/Detail.js
@@ -1,18 +1,67 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import { SafeAreaView, TouchableOpacity, ScrollView, StyleSheet, Text, View, Image, FlatList, TouchableOpacityBase } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import categoriesData from '../assets/data/categoriesData';
 import popularData from '../assets/data/popularData';
 import colors from '../assets/colors';
 
-const Detail=({route})=>{
+
+Feather.loadFont();
+MaterialCommunityIcons.loadFont();
+const Detail=({route, navigation})=>{
     const {item}=route.params;
 
     return(
-        <View>
-            <Text>Details Page !!!</Text>
+        <View style={styles.container}>
+        {/*Header*/}
+
+        <SafeAreaView>
+        <View style={styles.headerWrapper}>
+           <TouchableOpacity
+           onPress={()=> navigation.goBack()}>
+           <View style={styles.headerLeft}>
+                <Feather name="chevron-left" size={12} color={colors.textDark} />
+            </View>
+           </TouchableOpacity>
+            <View style={styles.headerRight}>
+                <MaterialCommunityIcons name="star" size={12} color={colors.white} />
+            </View>
+        </View>
+            
+       
+        </SafeAreaView>
+
+        
         </View>
     );
 } 
 export default Detail;
+const styles= new StyleSheet.create({
+    container:{
+        flex:1,
+
+    },
+    headerWrapper:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        paddingHorizontal:20,
+        paddingTop:45,
+
+    },
+    headerLeft:{
+        borderColor:colors.textLight,
+        borderWidth:2,
+        padding:12,
+        borderRadius:10,
+    },
+    headerRight:{
+        backgroundColor:colors.primary,
+        borderColor:colors.primary,
+        borderWidth:2,
+        padding:12,
+        borderRadius:10,
+    },
+
+});
