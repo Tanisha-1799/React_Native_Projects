13070c4270e4c226160d66187acd26a7978f5b59
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-29 18:07:12 +0530

    started developing UI for the Travel app

---
+++ TravelappUI/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import Apploading from "expo-app-loading";
+
+
+const getFonts = () =>
+  Font.loadAsync({
+    latoBlack: require("./assets/fonts/Lato-Black.ttf"),
+    latoBlackItalic: require("./assets/fonts/Lato-BlackItalic.ttf"),
+    latoBold: require("./assets/fonts/Lato-Bold.ttf"),
+    latoBoldItalic: require("./assets/fonts/Lato-BoldItalic.ttf"),
+    latoItalic: require("./assets/fonts/Lato-Italic.ttf"),
+    latoLight: require("./assets/fonts/Lato-Light.ttf"),
+    latoLightItalic: require("./assets/fonts/Lato-LightItalic.ttf"),
+    latoRegular: require("./assets/fonts/Lato-Regular.ttf"),
+    latoThin: require("./assets/fonts/Lato-Thin.ttf"),
+    latoThinItalic: require("./assets/fonts/Lato-ThinItalic.ttf"),
+
+
+    
+  });
+
+
+
+  export default function App() {
+    const [fontsloaded, setFontsLoaded] = useState(false);
+  
+    if (fontsloaded) {
+      return (
+       //you can return any component of your choice here
+        <View style={styles.container}>
+          <Text style={styles.Texts}>
+            Open up App.js to start working on your app!
+          </Text>
+         
+        </View>
+      );
+    } else {
+      return (
+        <Apploading
+          startAsync={getFonts}
+          onFinish={() => {
+            setFontsLoaded(true);
+          }}
+          onError={console.warn}
+        />
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    Texts: {
+      fontFamily: "latoBoldItalic",
+    },
+  });
