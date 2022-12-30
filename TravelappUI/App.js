632aec76db60f9ac347c84d75e54a03f8e543d67
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-30 18:58:57 +0530

    Designing the bottom tab for the first page

--- TravelappUI/App.js
+++ TravelappUI/App.js
@@ -10,9 +10,13 @@ import Details from './components/Details';
 import Home from './components/Home';
 import Liked from './components/Liked';
 import Profile from './components/Profile';
+import colors from './assets/colors/colors';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 
-
+Entypo.loadFont();
+MaterialCommunityIcons.loadFont();
 const getFonts = () =>
   Font.loadAsync({
     latoBlack: require("./assets/fonts/Lato-Black.ttf"),
@@ -36,10 +40,22 @@ const getFonts = () =>
 
   const TabNavigator=()=>{
     return(
-      <TabNavigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Liked" component={Liked} />
-        <Tab.Screen name="Profile" component={Profile} />
+      <TabNavigator
+        tabBarOptions={{
+          style:styles.tabBar,
+          activeTintColor:colors.orange,
+          inacctiveTintColor:colors.gray,
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon:({color})=> <Entypo name='home' size={32} color={color} />
+        }}/>
+        <Tab.Screen name="Liked" component={Liked} options={{
+          tabBarIcon:({color})=> <Entypo name='heart' size={32} color={color} />
+        }}/>
+        <Tab.Screen name="Profile" component={Profile} options={{
+          tabBarIcon:({color})=> <MaterialCommunityIcons name='account' size={32} color={color} />
+        }}/>
       </TabNavigator>
     );
   }
@@ -83,5 +99,10 @@ const getFonts = () =>
     },
     Texts: {
       fontFamily: "latoBoldItalic",
+    },
+    tabBar:{
+      backgroundColor:colors.white,
+      borderTopLeftRadius:20,
+      borderTopRightRadius:20,
     },
   });
