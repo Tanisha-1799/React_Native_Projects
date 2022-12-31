696d36e726323feb5e18e9aad6c7e61789102d27
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-31 18:35:03 +0530

    Designing the bottom tab for the first page

--- TravelappUI/App.js
+++ TravelappUI/App.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 import Apploading from "expo-app-loading";
+import {createStackNavigator} from '@react-navigation/stack';
 
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Details from './components/Details';
 import Home from './components/Home';
@@ -13,6 +14,7 @@ import Profile from './components/Profile';
 import colors from './assets/colors/colors';
 import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 Entypo.loadFont();
 MaterialCommunityIcons.loadFont();
@@ -34,60 +36,98 @@ const getFonts = () =>
   });
  
   
- 
+  const Stack = createStackNavigator();
   const Tab = createBottomTabNavigator();
 
-  const TabNavigator=()=>{
-    return(
-      <TabNavigator
+  const TabNavigator = () => {
+    return (
+      <Tab.Navigator
         tabBarOptions={{
-          style:styles.tabBar,
-          activeTintColor:colors.orange,
-          inacctiveTintColor:colors.gray,
-          showLabel:false,
-        }}
-      >
-        <Tab.Screen name="Home" component={Home} options={{
-          tabBarIcon:({color})=> <Entypo name='home' size={32} color={color} />
-        }}/>
-        <Tab.Screen name="Liked" component={Liked} options={{
-          tabBarIcon:({color})=> <Entypo name='heart' size={32} color={color} />
-        }}/>
-        <Tab.Screen name="Profile" component={Profile} options={{
-          tabBarIcon:({color})=> <MaterialCommunityIcons name='account' size={32} color={color} />
-        }}/>
-      </TabNavigator>
+          style: styles.tabBar,
+          activeTintColor: colors.orange,
+          inactiveTintColor: colors.gray,
+          showLabel: false,
+          
+          
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Entypo name="home" size={32} color={color} />
+            ),
+            headerShown:false,
+          }}
+        />
+        <Tab.Screen
+          name="Liked"
+          component={Liked}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Entypo name="heart" size={32} color={color} />
+            ),
+            headerShown:false,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons name="account" size={32} color={color} />
+            ),
+            headerShown:false,
+          }}
+        />
+      </Tab.Navigator>
     );
+  };
+  
+
+  export default function App() {
+    const [fontsloaded, setFontsLoaded] = useState(false);
+  
+    if (fontsloaded) {
+      return (
+       
+        <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="TabNavigator"
+            component={TabNavigator}
+            options={{headerShown:false}}
+          />
+          <Stack.Screen
+            name="Details"
+            component={Details}
+            options={{headerShown:false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+       
+      );
+    } else {
+      return (
+        <Apploading
+          startAsync={getFonts}
+          onFinish={() => {
+            setFontsLoaded(true);
+          }}
+          onError={console.warn}
+        />
+      );
+    }
   }
+  const styles = StyleSheet.create({
+   
+    tabBar:{
+      backgroundColor:colors.white,
+      borderTopLeftRadius:20,
+      borderTopRightRadius:20,
+     
+    },
+  });
 
 
 
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="TabNavigator" 
-        component={TabNavigator}
-        options={{headerShown:false}} />
-        <Stack.Screen 
-        name="Details" 
-        component={Details}
-        options={{headerShown:false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
-
-const styles = StyleSheet.create({
- 
-  tabBar:{
-    backgroundColor:colors.white,
-    borderTopLeftRadius:20,
-    borderTopRightRadius:20,
-  },
-});
