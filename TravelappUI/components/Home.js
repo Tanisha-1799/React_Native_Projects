10955835bf45c7f705bafdbda2d9ca3b3ec7126d
Tanisha-1799 <triveditanisha01171999@gmail.com>
2022-12-31 19:19:20 +0530

    finished designing the header for the home page

--- TravelappUI/components/Home.js
+++ TravelappUI/components/Home.js
@@ -1,14 +1,67 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView, Image, Text, View, Button } from 'react-native';
 import React from "react";
+import colors from '../assets/colors/colors';
+import Feather from 'react-native-vector-icons/Feather';
+import activitiesData from '../assets/data/activitiesData';
+import discoverCategoriesData from '../assets/data/discoverCategoriesData';
+import discoverData from '../assets/data/discoverData';
+import learnMoreData from '../assets/data/learnMoreData';
+import prr2 from '../assets/images/prr2.jpg';
+
+
+Feather.loadFont();
+
 const Home=({navigation})=>{
     return (
-        <View>
-            <Text>Home</Text>
-            <Button onPress={()=> navigation.navigate("Details")}
-                title="Click Me!!!"
-            />
+        <View style={styles.container}>
+        <ScrollView>
+
+                    {/*Header */}
+            <SafeAreaView>
+                <View style={styles.menuWrapper}>
+                    <Feather 
+                        name="menu" 
+                        size={32} 
+                        color={colors.black} 
+                        style={styles.menuIcon}
+                    />
+                    <Image source={prr2} style={styles.profileImage}/>
+
+                </View>
+
+            </SafeAreaView>
+
+                    {/* Dicover */}
+
+
+        </ScrollView>
+            
         </View>
         
     );
 };
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        color:colors.white,
+
+    },
+    menuWrapper:{
+        marginHorizontal:20,
+        marginTop:30,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+    },
+    menuIcon:{},
+    profileImage:{
+        width:52,
+        height:52,
+        borderRadius:10,
+       
+    },
+
+
+});
 export default Home;
