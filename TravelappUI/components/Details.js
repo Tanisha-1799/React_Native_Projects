fa67092cbf89236c4e39417aa9e8df79998f46b5
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-03 18:40:57 +0530

    started designing the details page of the app

--- TravelappUI/components/Details.js
+++ TravelappUI/components/Details.js
@@ -1,11 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground, Text, View, Dimensions } from 'react-native';
 import React from "react";
-const Details=()=>{
+import colors from '../assets/colors/colors';
+
+
+const height=Dimensions.get('window').height;
+const Details=({route, navigation})=>{
+    const {item}=route.params
     return (
-        <View>
-            <Text>Details</Text>
+        <View style={styles.container}>
+           <ImageBackground source={item.imageBig} style={styles.backgroundImage}></ImageBackground>
+           <View style={styles.descriptionWrapper}>
+
+           </View>
         </View>
         
     );
 };
 export default Details; 
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:colors.white,
+    },
+    backgroundImage:{
+        height:height*0.6,
+    },
+    descriptionWrapper:{},
+});
