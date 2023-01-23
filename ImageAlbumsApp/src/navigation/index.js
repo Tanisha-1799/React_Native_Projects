fbb32ac2555410622e6c3e39147ac82889d72593
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-23 18:11:52 +0530

    Making the required components

--- ImageAlbumsApp/src/navigation/index.js
+++ ImageAlbumsApp/src/navigation/index.js
@@ -3,8 +3,14 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Albums from '../components/Albums';
 import SharedAlbum from '../components/SharedAlbum';
 
+const Stack=createStackNavigator();
+
 const Navigator = () => {
     return (
+        <Stack.Navigator>
+            <Stack.Screen name='Albums' component={Albums} />
+            <Stack.Screen name='Shared Album' component={SharedAlbum} />
+        </Stack.Navigator>
         
     );
 };
