2ce3f5a99738530915b0b5a51e4b1543d0131d33
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-23 18:08:38 +0530

    Making the required components

---
+++ ImageAlbumsApp/src/navigation/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Albums from '../components/Albums';
+import SharedAlbum from '../components/SharedAlbum';
+
+const Navigator = () => {
+    return (
+        
+    );
+};
+
+export default Navigator;
