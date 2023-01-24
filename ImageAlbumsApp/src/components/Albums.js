11cd957448ac061cea85bd3bc0efbd33043564ed
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-24 18:49:31 +0530

    Designing the first page

--- ImageAlbumsApp/src/components/Albums.js
+++ ImageAlbumsApp/src/components/Albums.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { View, Text, Pressable, ScrollView, StyleSheet } from 'react-native';
 import theme from '../../assets/themes/index';
+import albumPage from '../../assets/data/albumPage';
 
 const Albums=({navigation})=>{
     return(
         <>
           <ScrollView>
             <View style={styles.albumContainer}>
+            {albumPage.map((item)=>
+              
+                
+                    <View>
+                    <Text>{item.title}</Text>
+                    </View>
+                
+              
+            )}
 
             </View>
           </ScrollView>  
@@ -16,7 +26,7 @@ const Albums=({navigation})=>{
 
 const styles=StyleSheet.create({
     albumContainer:{
-        marginBottom:theme.spacing.l,
+        //marginBottom:theme.spacing.l,
     },
 })
 
