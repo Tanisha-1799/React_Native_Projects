679628983f2021a4bbbdcfe646ecb5313f6f2032
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-24 19:22:59 +0530

    Designing the albums page making reusable components

--- ImageAlbumsApp/src/components/Card.js
+++ ImageAlbumsApp/src/components/Card.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import theme from '../../assets/themes';
+import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 
 const Card = ({item}) => {
     return (
-        <View>
-            <Text>{item.title}</Text>
-        </View>
+        <ImageBackground
+            source={item.background}
+            style={styles.imageBackground}
+        >
+            <Pressable>
+                <View style={styles.imageContentContainer}>
+                    <View>
+                        <Text style={styles.imageTitle}>{item.title}</Text>
+                        <Text style={styles.imageSubtitle}>{`Created by ${item.user}`}</Text>
+                    </View>
+
+                    <View>
+                        {/*Avatars go here !!!*/}
+                    </View>
+
+                </View>
+            </Pressable>
+        </ImageBackground>
     );
 };
+
+const styles=StyleSheet.create({
+    imageBackground:{
+        resizeMode:'cover',
+        overflow:'hidden',
+        height:theme.imageHeight.s,
+        marginTop:theme.spacing.m,
+        marginHorizontal:theme.spacing.m,
+        paddingHorizontal:theme.spacing.m,
+        
+    },
+    imageContentContainer:{},
+    imageTitle:{},
+    imageSubtitle:{},
+})
 
 export default Card;
