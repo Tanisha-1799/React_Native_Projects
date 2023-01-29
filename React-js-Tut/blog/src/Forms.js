04cb8cd89d42c4d5d2b0390faa0fcc6d5c9f2656
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-30 00:40:18 +0530

    Learning form validation  in react

--- React-js-Tut/blog/src/Forms.js
+++ React-js-Tut/blog/src/Forms.js
@@ -4,13 +4,33 @@ export default class Forms extends React.Component{
     constructor(){
         super();
         this.state={
-            user:null,
-            password:null
+            user:'',
+            password:'',
+            userError:'',
+            passwordError:''
 
         }
     }
+    valid(){
+        if(this.state.user.includes('@')&&this.state.password.length<5){
+            this.setState({userError:"Invalid Name"})
+            this.setState({passwordError:"Password length should be more than 5"})
+        }
+        else if(this.state.user.includes('@')){
+            this.setState({userError:"Invalid Name"})
+            }
+        else if(this.state.password.length<5){
+            
+            this.setState({passwordError:"Password length should be more than 5"})
+        }else{
+            return true;
+        }
+    }
     submit() {
-        console.warn(this.state);
+       //validation of form
+       if(this.valid()){
+        alert("Form submitted successfully !!");
+       }
         
     }
     render(){
@@ -19,13 +39,16 @@ export default class Forms extends React.Component{
                 <h1>Form Handling</h1>
                 <input type="text" name="user" 
                 onChange={(e)=>{this.setState({user:e.target.value})}} />
-                <br /><br />
-                <p></p>
+                <br />
+                <p>{this.state.userError}</p>
+                <br />
                 <input type="text" name="password" 
                 onChange={(e)=>{this.setState({password:e.target.value})}} />
-                <br /><br />
+                <br />
+                <p>{this.state.passwordError}</p>
+                <br />
                 <button onClick={()=>{this.submit()}}>Submit</button>
-                <p></p>
+                
             </div>
         )
     }
