0e0749ccf77dd9edcd1a9ddcc6036cfc21bba46a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-31 00:17:32 +0530

    Fetching api data in react

--- React-js-Tut/blog/src/App.js
+++ React-js-Tut/blog/src/App.js
@@ -5,28 +5,59 @@ import Profile from './Profile';
 import About from './About';
 import React,{useState,useEffect} from 'react';
 import Forms from './Forms';
+import WelcomePage from './WelcomePage';
+import ContactPage from './ContactPage';
+import HelpPage from './HelpPage';
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom'
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      users:null
+    }
+  }
+
+  //Calling Api
+  componentDidMount(){
+    fetch("https://reqres.in/api/users").then((res)=>{
+      res.json().then((result)=>{
+        console.warn(result)
+      })
+    })
+  }
+
   //making use of a basic hook
   //useState
-  const [count,setCount]=useState(0);
+  //const [count,setCount]=useState(0);
   //Making use of useEffect hook
-  useEffect(()=>{
-    console.warn(count);
+  //useEffect(()=>{
+    //console.warn(count);
     //if you want that the function should update only once
     //then leave the array empty in the second parameter
-  },[count==5]);
-  
+  //},[count==5]);
+  render(){
   return (
-    <div className="App">  
-   
-      <h1>Form in react</h1>
-      <Forms />
-
-     
+    <div>  
+      {/**<Router>
+      {/**<WelcomePage />
+    <ContactPage />
+    <HelpPage />   
+    <Link to="/">Welcome </Link>
+    <Link to="/contact"> Contact </Link>
+    <Link to="/help"> Help</Link>
+    <Routes>
+    <Route path="/" element={<WelcomePage />} />
+    <Route path="/contact" element={<ContactPage />} />
+    <Route path="/help" element={<HelpPage />} />
+    </Routes>
+    
+    </Router>   */}
+    <h1>Fetching Api Data</h1>
      
     </div>
   );
+  }
 }
 
 export default App;
---
+++ React-js-Tut/blog/src/HelpPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class HelpPage extends Component {
+  render() {
+    return (
+      <div>HelpPage</div>
+    )
+  }
+}
