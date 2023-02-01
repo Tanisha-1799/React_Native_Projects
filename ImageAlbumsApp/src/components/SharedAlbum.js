78f382213ce8c4bb6063e4fd9e1c7653f2cf9bd3
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-01 13:07:54 +0530

    Designing the second page

--- ImageAlbumsApp/src/components/SharedAlbum.js
+++ ImageAlbumsApp/src/components/SharedAlbum.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import SharedAlbumTitleCard from './SharedAlbumTitleCard';
+import Avatars from './Avatars';
 
 const SharedAlbum = ({route}) => {
 const {album}=route.params;
@@ -8,10 +9,23 @@ const {album}=route.params;
     return (
        <ScrollView>
             <SharedAlbumTitleCard album={album} />
+            <View style={Styles.avatarContainer}>
+            <Avatars avatars={album.avatars} />
+            <Text>{`${album.avatars.length} people`}</Text>
+
+            </View>
 
        </ScrollView>
         
     );
 };
+
+const styles=StyleSheet.create({
+    avatarContainer:{
+        flexDirection:'row',
+        alignItems:'center',
+        
+    }
+})
 
 export default SharedAlbum;
