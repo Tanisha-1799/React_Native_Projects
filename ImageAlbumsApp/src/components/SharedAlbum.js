691782f5eeedf4c5a1eb7abf7b66b75627acc8a8
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-01 13:16:55 +0530

    Completed designing the avatar section

--- ImageAlbumsApp/src/components/SharedAlbum.js
+++ ImageAlbumsApp/src/components/SharedAlbum.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import SharedAlbumTitleCard from './SharedAlbumTitleCard';
 import Avatars from './Avatars';
+import theme from '../../assets/themes';
 
 const SharedAlbum = ({route}) => {
 const {album}=route.params;
@@ -24,7 +25,14 @@ const styles=StyleSheet.create({
     avatarContainer:{
         flexDirection:'row',
         alignItems:'center',
-        
+        justifyContent:'space-between',
+        backgroundColor:theme.colors.lightGray,
+        marginVertical:theme.spacing.l,
+        marginHorizontal:theme.spacing.m,
+        paddingHorizontal:theme.spacing.m,
+        paddingVertical:theme.spacing.xs,
+        borderRadius:theme.borderRadius.m,
+
     }
 })
 
