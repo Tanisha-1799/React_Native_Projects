e56268d30cfd69ad91d00b727a29d17afec5cca0
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-01 14:49:21 +0530

    Making the bottom sheet

--- ImageAlbumsApp/src/components/Albums.js
+++ ImageAlbumsApp/src/components/Albums.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { View, Text, Pressable, ScrollView, StyleSheet } from 'react-native';
+import React, { useLayoutEffect, useRef } from 'react';
+import { View, Text, Pressable, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import theme from '../../assets/themes/index';
 import albumPage from '../../assets/data/albumPage';
 import Card from './Card';
 import Seperator from './Seperator';
+import Feather from '@expo/vector-icons/Feather';
+import BottomSheet from 'reanimated-bottom-sheet'; 
+import BottomSheetContent from './BottomSheetContent';
 
-const Albums=({navigation})=>{
+const Albums=({navigation, navigation:{ setOptions } })=>{
+
+    //Making sheetRef for bottom sheet
+    const sheetRef=useRef();
+
+
+    //We are going to use the header component that is provided by react navigation
+    //Then we will just customize this header
+    //we want to do this as soon as the component is mounted 
+
+    useLayoutEffect(()=>{
+      setOptions({
+        headerRight:()=>(
+          <TouchableOpacity onPress={()=>console.log("Hello")}>
+          <View style={styles.openSheetButton}>
+           <Feather name="plus" size={16} color={theme.colors.white}/>
+          </View>
+
+          </TouchableOpacity>
+        )
+      })
+    })
+    //making function to render the content of bottom sheet
+    const renderBottomSheetContent=()=> <BottomSheetContent />
+
     return(
         <>
           <ScrollView>
@@ -22,7 +49,14 @@ const Albums=({navigation})=>{
             )}
 
             </View>
-          </ScrollView>  
+          </ScrollView>
+          <BottomSheet 
+            ref={sheetRef}
+            snapPoints={[250,0]}
+            initialSnap={1}
+            borderRadius={10}
+            renderContent={renderBottomSheetContent}
+          />  
         </>
     );
 };
@@ -30,6 +64,15 @@ const Albums=({navigation})=>{
 const styles=StyleSheet.create({
     albumContainer:{
         marginBottom:theme.spacing.l,
+    },
+    openSheetButton:{
+      width:32,
+      height:32,
+      backgroundColor:theme.colors.primary,
+      marginRight:theme.spacing.m,
+      borderRadius:16,
+      justifyContent:'center',
+      alignItems:'center',
     },
 })
 
---
+++ ImageAlbumsApp/src/components/BottomSheetContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const BottomSheetContent = () => {
+    return (
+        <View>
+            <Text>I am bottom sheet</Text>
+        </View>
+        
+    );
+};
+
+export default BottomSheetContent;
