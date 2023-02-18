2e1e6c373a5171c849e1c3cad1904980fa2c8051
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-18 18:47:20 +0530

    Learning Pure Component

---
+++ React-js-Tut/blog/src/PureComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class PureComponent extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Pure Component !</h1>
+            </div>
+        )
+    }
+};
+
+export default PureComponent;
